86f9f131eb0ba92263b5db651c9ff5b3fd076aef
Long Pham <long29031997@gmail.com>
2023-12-14 01:41:28 +0700

    finish Games: Menu exercise

--- courses/mix_projects/games/lib/games.ex
+++ courses/mix_projects/games/lib/games.ex
@@ -1,18 +1,18 @@
 defmodule Games do
-  @moduledoc """
-  Documentation for `Games`.
-  """
+  def main(args) do
+    user_choice= IO.gets("""
+    What game would you like to play?
+    1. Guessing Game
+    2. Rock Paper Scissors
+    3. Wordle
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Games.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    enter "stop" to exit
+    """) |> String.trim()
+    case user_choice do
+      "1"->IO.puts(Games.GuessingGame.play())
+      "2"->IO.puts(Games.RockPaperScissors.play())
+      "3"->IO.puts(Games.Wordle.play())
+      "stop" -> IO.puts("Goodbye!")
+    end
   end
 end
