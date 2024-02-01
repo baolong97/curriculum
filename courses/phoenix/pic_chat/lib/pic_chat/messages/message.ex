c8d283745e7dbc1dc0f084271ed89a93124fd034
Long Pham <long29031997@gmail.com>
2024-02-01 18:28:37 +0700

    finish PicChat: Authentication reading

--- courses/phoenix/pic_chat/lib/pic_chat/messages/message.ex
+++ courses/phoenix/pic_chat/lib/pic_chat/messages/message.ex
@@ -4,14 +4,15 @@ defmodule PicChat.Messages.Message do
 
   schema "messages" do
     field :content, :string
+    belongs_to :user, PicChat.Accounts.User
 
-    timestamps(type: :utc_datetime)
+    timestamps()
   end
 
   @doc false
   def changeset(message, attrs) do
     message
-    |> cast(attrs, [:content])
+    |> cast(attrs, [:content, :user_id])
     |> validate_required([:content])
   end
 end
